bfb018f0a82eb5111cca82d86cb52cddade4e52b
GOWTHAM BHASKAR.DAMARASINGU <dgowthambhaskar@gmail.com>
2024-11-17 17:19:52 +0530

    PHP Project

---
+++ my php project/index.php
@@ -0,0 +1,171 @@
+﻿<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>BUS TICKET RESERVATION SYSTEM</title>
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" type="text/css" href="css/color/orange.css">
+</head>
+<body >
+<div class="preloader">
+<div class="loader theme_background_color">
+<span></span>
+</div>
+</div>
+<div class="wrapper">
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="index.html">BUS<span class="themecolor">-TICKET-</span>RESERVATION</a>
+</div>
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav navbar-right">
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+<li><a href="#two" class="page-scroll"><h3>About</h3></a></li>
+<li><a href="#three" class="page-scroll"><h3>Contact</h3></a></li>
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+</ul>
+</div>
+</div>
+</nav>
+<section class="main-heading" id="home">
+<div class="overlay">
+<div class="container">
+<div class="row">
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" BUS TICKET, BUS TICKET, BUS TICKET"></span></h1>
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" RESERVATION SYSTEM, RESERVATION SYSTEM, RESERVATION SYSTEM"></span></h1>
+<p class="main-heading-text">WELCOME TO BUS TICKET RESERVATION SYSTEM<br/></p>
+<div class="btn-bar">
+<a href="reserved.php" class="btn btn-custom theme_background_color">RESERVE NOW</a>
+</div>
+</div>
+</div>
+</div>
+</div>  
+</section>
+<section class="main-heading" id="home">
+<div class="overlay">
+<div class="container">
+<div class="row">
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+<h1 class="main-heading-title">We are Creative</h1>
+<p class="main-heading-text">WELCOME TO BUS TICKET RESERVATION SYSTEM
+</p>
+<div class="btn-bar">
+<a href="#" class="btn btn-custom theme_background_color">Get Started</a>
+</div>
+</div>
+</div>
+</div>
+</div>  
+</section>
+<section class="aboutus white-background black" id="one">
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-center black">
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+<p class="a-slog">A SOFTWARE PROJECT DESIGNED FOR EDUCATIONAL PURPOSE BY A STUDENT {DAMARASINGU.GOWTHAM BHASKAR}</p> 
+</div>
+</div>
+<div class="gap">
+</div>
+<div class="row about-box">
+<div class="col-sm-4 text-center">
+<div class="margin-bottom">
+<i class="fa fa-newspaper-o"></i>
+<h4>POWER OF FLEXIBILITY</h4>
+<p class="black">We are flexible to any kind of environments and we will try to provide you the best journey experience.</p>
+</div>
+</div>
+<div class="col-sm-4 about-line text-center">
+<div class="margin-bottom">
+<i class="fa fa-diamond"></i>
+<h4>FULLY RESPONSIVE</h4>
+<p class="black">We respect our customers and we will try to give you the quick response from our side as soon as possible.</p>
+</div>
+</div>
+<div class="col-sm-4 text-center">
+<div class="margin-bottom">
+<i class="fa fa-area-chart"></i>
+<h4>GREAT WEB IDEAS</h4>
+<p class="black">We have a strong developing team to work on new ideas.</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<div class="overlay">
+<div class="container">
+<div class="row">
+<article class="col-md-12 text-center">
+<div class="intermediate-container">
+<div class="subheading">
+<h4>Are You Ready To <span class="themecolor">Enjoy?</span></h4>
+</div>
+<div class="heading">
+<h2>CONTACT US THROUGH HERE :)</h2>
+<h6> Mobile:- (+91)9550966764</h6>
+<h6>g.mail:- gowthamdamarasingu@gmail.com</h6>
+</div>
+<div class="">
+<a class="btn btn-custom-outline" href="#"><span>Get Started</span></a>
+</div>
+</div>
+</article>
+</div>
+</div>
+</div>
+<section class="sub-form text-center" id="eight">
+<div class="container">
+<div class="col-md-12">
+<h3 class="title">Encourage our <span class="themecolor">Website</span></h3>
+<p class="lead">Thankyou for choosing our website.</p>
+</div> 
+<div class="row">
+<div class="col-md-3 col-sm-3"></div>
+<div class="col-md-6 center-block col-sm-6 ">
+<form id="mc-form">
+<div class="input-group">
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+<span class="input-group-btn">
+<button type="submit" class="btn btn-default">SUBSCRIBE <i class="fa fa-envelope"></i> </button>
+</span> </div>
+<label for="mc-email" id="mc-notification"></label>
+</form>
+</div>
+</div>
+</div>
+</section>
+</div>
+<script src="library/modernizr.custom.97074.js"></script>
+<script src="library/jquery-1.11.3.min.js"></script>
+<script src="library/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+<script src="library/vegas/vegas.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/typed.js"></script>
+<script src="js/fappear.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+<script src="js/common.js"></script>
+</body>
+</html>
